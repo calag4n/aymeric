e7fc90fa84853032778bec39e8451af133aab7f5
Aymeric Basset <aymeric.basset@gmail.com>
2020-11-10 17:45:45 +0100

    adding content in experience and skills

--- src/experience/app.js
+++ src/experience/app.js
@@ -3,6 +3,7 @@ import ensta from './ensta.png'
 import tel from './telecom.png'
 import ilb from './ilb.png'
 import kcl from './kcl.png'
+import champo from './champo.png'
 import { VerticalTimeline, VerticalTimelineElement }  from 'react-vertical-timeline-component';
 import 'react-vertical-timeline-component/style.min.css';
 import SchoolIcon from '@material-ui/icons/School';
@@ -18,81 +19,93 @@ export default function experience() {
         
       <VerticalTimelineElement
         className="vertical-timeline-element--work"
-        contentStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
-        contentArrowStyle={{ borderRight: '7px solid  rgb(33, 150, 243)' }}
-        date="2011 - present"
-        iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
-        icon={<WorkIcon />}
+        date="Since September 2020"
+        iconStyle={{ background: '#f50057', color: '#fff' }}
+        icon={<SchoolIcon />}
       >
-        <h3 className="vertical-timeline-element-title">Creative Director</h3>
-        <h4 className="vertical-timeline-element-subtitle">Miami, FL</h4>
-        <img src={kcl} style={{float:'right', width:'100px'}} ></img>
+        <h3 className="vertical-timeline-element-title">MSc Artificial Intelligence</h3>
+        <h4 className="vertical-timeline-element-subtitle">Telecom Paris</h4>
+        <a href="https://www.telecom-paris.fr/en/post-masters-degree/all-post-masters-degree/artificial-intelligence" target="_blank"><img src={tel} style={{float:'right', width:'20%'}} ></img></a>
         <p>
-          Creative Direction, User Experience, Visual Design, Project Management, Team Leading Creative Direction, User Experience, Visual Design, Project Management, Team Leading Creative Direction, User Experience, Visual Design, Project Management, Team Leading Creative Direction, User Experience, Visual Design, Project Management, Team Leading
+      Last year advanced specialization resulting from the partnership between Ensta Paris and Telecom Paris.
         </p>
-      </VerticalTimelineElement>
-      <VerticalTimelineElement
-        className="vertical-timeline-element--work"
-        date="2010 - 2011"
-        iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
-        icon={<WorkIcon />}
-      >
-        <h3 className="vertical-timeline-element-title">Art Director</h3>
-        <h4 className="vertical-timeline-element-subtitle">San Francisco, CA</h4>
-        <img src={ensta} style={{float:'right', width:'100px'}} ></img>
         <p>
-          Creative Direction, User Experience, Visual Design, SEO, Online Marketing
-        </p>
-      </VerticalTimelineElement>
-      <VerticalTimelineElement
-        className="vertical-timeline-element--work"
-        date="2008 - 2010"
-        iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
-        icon={<WorkIcon />}
-      >
-        <h3 className="vertical-timeline-element-title">Web Designer</h3>
-        <h4 className="vertical-timeline-element-subtitle">Los Angeles, CA</h4>
-        <img src={tel} style={{float:'right', width:'100px'}} ></img>
-        <p>
-          User Experience, Visual Design
+        Coursework includes: Deep Learning, Reinforcement Learning, Metaheuristic Optimization, Natural Language Processing, Statistical Learning, Multi-agent Programming, GPGPU Programming, Perception for autonomous systems, Large-scale statistical learning
         </p>
 
       </VerticalTimelineElement>
       <VerticalTimelineElement
         className="vertical-timeline-element--work"
-        date="2006 - 2008"
+        date="March 2020 - August 2020"
         iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
         icon={<WorkIcon />}
       >
-        <h3 className="vertical-timeline-element-title">Web Designer</h3>
-        <h4 className="vertical-timeline-element-subtitle">San Francisco, CA</h4>
-        <img src={ilb} style={{float:'right', width:'100px'}} ></img>
+        <h3 className="vertical-timeline-element-title">Research Assistant</h3>
+        <h4 className="vertical-timeline-element-subtitle">King's College London</h4>
+        <a href="https://www.kcl.ac.uk/psychology-systems-sciences/about/departments/biostatistics-and-health-informatics" target="_blank"><img src={kcl} style={{float:'right', width:'20%'}} ></img></a>
         <p>
-          User Experience, Visual Design
+        <ul>
+          <li>Scrapped medical web data to create a comprehensive disease database and applied classification and clustering techniques on it.</li>
+          <li>Built a simplified interface for doctors to generate randomized patients and test diagnostic tools.</li>
+          <li>Worked on improving state-of-the-art medical annotation tool for drug/dosage NER</li>
+        </ul> 
+        </p>
+      </VerticalTimelineElement>
+      <VerticalTimelineElement
+        className="vertical-timeline-element--work"
+        date="Septembre 2019 - March 2020"
+        iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+        icon={<WorkIcon />}
+      >
+        <h3 className="vertical-timeline-element-title">Data Scientist</h3>
+        <h4 className="vertical-timeline-element-subtitle">Institut Louis Bachelier, Paris</h4>
+        <a href="https://www.institutlouisbachelier.org/en/homepage/" target="_blank" ><img src={ilb} style={{float:'right', width:'20%'}} ></img></a>
+        <p>
+          <ul>
+            <li>Researched GAN applications for financial time series</li>
+            <li> Prediction of food insecurity due to droughts in Africa using spatial and temporal data </li>
+            <li> Built a tool to complete missing values in industrial databases using sequential models with customizable parameters</li>
+          </ul>
+        </p>
+
+      </VerticalTimelineElement>
+      <VerticalTimelineElement
+        className="vertical-timeline-element--work"
+        date="2017 - 2021"
+        iconStyle={{ background: '#f50057', color: '#fff' }}
+        icon={<SchoolIcon />}
+      >
+        <h3 className="vertical-timeline-element-title">Msc Computer Science</h3>
+        <h4 className="vertical-timeline-element-subtitle">Ensta Paris</h4>
+        <a href="https://www.ensta-paristech.fr/en/home" target="_blank"><img src={ensta} style={{float:'right', width:'20%'}} ></img></a>
+        <p>
+        ENSTA Paris belongs to the foremost graduate schools of engineering in France called "Grandes Ecoles" and awards the french "Diplôme d'Ingénieur". 
+        </p>
+        <p>
+        Coursework includes core scientific training in:
+        <ul>
+          <li>Mechanics (Solid, Fluid, Quantum)</li>
+          <li>Mathematics (Statistics & Probability, Calculus, Geometry)</li>
+          <li>Computer Sciences (Signal Processing, Software Engineering, Databases, Parallel Programming)</li>
+          <li>Biology (introduction to Genetic and Immunology)</li>
+        </ul>
         </p>
       </VerticalTimelineElement>
       <VerticalTimelineElement
         className="vertical-timeline-element--education"
-        date="April 2013"
-        iconStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }}
+        date="2014 - 2017"
+        iconStyle={{ background: '#f50057', color: '#fff' }}
         icon={<SchoolIcon />}
       >
-        <h3 className="vertical-timeline-element-title">Content Marketing for Web, Mobile and Social Media</h3>
-        <h4 className="vertical-timeline-element-subtitle">Online Course</h4>
+        <h3 className="vertical-timeline-element-title">Classe Préparatoire Aux Grandes Écoles</h3>
+        <h4 className="vertical-timeline-element-subtitle">Champollion, Grenoble</h4>
+        <a href="https://en.wikipedia.org/wiki/Classe_pr%C3%A9paratoire_aux_grandes_%C3%A9coles" target="_blank"><img src={champo} style={{float:'right', width:'20%'}} ></img></a>
+
         <p>
-          Strategy, Social Media
+        2 years of very demanding scientific courses with prior selection before competing in nationwide exams to enter Top French Graduate Schools of Engineering.
         </p>
-      </VerticalTimelineElement>
-      <VerticalTimelineElement
-        className="vertical-timeline-element--education"
-        date="November 2012"
-        iconStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }}
-        icon={<SchoolIcon />}
-      >
-        <h3 className="vertical-timeline-element-title">Agile Development Scrum Master</h3>
-        <h4 className="vertical-timeline-element-subtitle">Certification</h4>
         <p>
-          Creative Direction, User Experience, Visual Design
+Ranked 303 (top 5% overall) 
         </p>
       </VerticalTimelineElement>
     </VerticalTimeline>
--- src/skills.js
+++ src/skills.js
@@ -110,20 +110,77 @@ export default function CustomizedTreeView() {
       defaultExpandIcon={<PlusSquare />}
       defaultEndIcon={<CloseSquare />}
     >
-      <StyledTreeItem nodeId="1" label="My Skills">
-        <StyledTreeItem nodeId="2" label="Mathematics" />
-        <StyledTreeItem nodeId="3" label="Computer Science">
-          <StyledTreeItem nodeId="6" label="Hello" />
-          <StyledTreeItem nodeId="7" label="Sub-subtree with children">
-            <StyledTreeItem nodeId="9" label="Child 1" />
-            <StyledTreeItem nodeId="10" label="Child 2" />
-            <StyledTreeItem nodeId="11" label="Child 3" />
-          </StyledTreeItem>
-          <StyledTreeItem nodeId="8" label="Hello" />
+      <StyledTreeItem nodeId="1" label="Datascience Skills">
+
+        <StyledTreeItem nodeId="2" label="Programming" >
+
+          <StyledTreeItem odeId="9" label="Python"/>
+          <StyledTreeItem odeId="10" label="Java"/>
+          <StyledTreeItem odeId="11" label="C++"/>
+          <StyledTreeItem odeId="12" label="SQL"/>
+          <StyledTreeItem odeId="13" label="Javascript"/>
+          <StyledTreeItem odeId="14" label="Software Engineering"/>
+
         </StyledTreeItem>
-        <StyledTreeItem nodeId="4" label="Other" />
-        <StyledTreeItem nodeId="5" label="Something something" />
+
+        <StyledTreeItem nodeId="3" label="Frameworks and Libraries">
+
+          <StyledTreeItem nodeId="15" label="Pytorch" />
+          <StyledTreeItem nodeId="16" label="Keras"/>
+          <StyledTreeItem nodeId="17" label="Scikit-Learn" />
+          <StyledTreeItem nodeId="18" label="XGBoost" />
+          <StyledTreeItem nodeId="19" label="Pandas" />
+          <StyledTreeItem nodeId="20" label="Numpy" />
+
+        </StyledTreeItem>
+
+        <StyledTreeItem nodeId="4" label="Toolkit">
+
+          <StyledTreeItem nodeId="21" label="Statistics for Machine learning" />
+          <StyledTreeItem nodeId="22" label="Regularization"/>
+          <StyledTreeItem nodeId="23" label="Multiple Inputation by Chained Equations " />
+          <StyledTreeItem nodeId="24" label="Synthetic Minority Oversampling Techniques" />
+          <StyledTreeItem nodeId="25" label="Model Interpretability" />
+          <StyledTreeItem nodeId="26" label="Feature Selection" />
+          <StyledTreeItem nodeId="27" label="Metaheuristic Optimization" />
+
+
+          
+        </StyledTreeItem>
+
+        <StyledTreeItem nodeId="5" label="Techniques">
+
+          <StyledTreeItem nodeId="28" label="Convolutional Neural Networks" />
+          <StyledTreeItem nodeId="29" label="Recurrent Neural Networks"/>
+          <StyledTreeItem nodeId="30" label="Multilayer Perceptron" />
+          <StyledTreeItem nodeId="31" label="Natural Language Processing" />
+          <StyledTreeItem nodeId="32" label="Reinforcement Learning" />
+          <StyledTreeItem nodeId="33" label="Generative Adversarial Models" />
+
+        </StyledTreeItem>
+
+        <StyledTreeItem nodeId="6" label="Cloud Computing">
+
+          <StyledTreeItem nodeId="34" label="Amazone Web Services" />
+
+        </StyledTreeItem>
+
+        <StyledTreeItem nodeId="7" label="Version Control">
+
+          <StyledTreeItem nodeId="35" label="GIT" />
+          
+        </StyledTreeItem>
+
+        <StyledTreeItem nodeId="8" label="Other">
+
+          <StyledTreeItem nodeId="36" label="Linux OS" />
+          <StyledTreeItem nodeId="37" label="Regex" />
+          <StyledTreeItem nodeId="38" label="Web Scrapping" />
+          
+        </StyledTreeItem>
+
       </StyledTreeItem>
+
     </TreeView>
   );
 }
